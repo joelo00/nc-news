d8ea27374e0cefffa02883709297fca21bfd4d17
Joe Lockhart <joe.lockhart0@gmail.com>
2023-10-23 10:57:25 +0100

    View all articles button added, and loading screen

---
+++ src/axios.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const newsAPI = axios.create({
+    baseURL: 'https://nc-news-hi1e.onrender.com/api'
+})
+
+export function getAllArticles() {
+    return newsAPI.get('/articles?limit=1000')
+
+}
